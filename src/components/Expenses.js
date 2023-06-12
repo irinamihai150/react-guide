3841712fa9f38e7f64685ace4415e6ff2002c3e6
irinamihai150 <irinamihai150@gmail.com>
2023-06-12 10:25:35 +0100

    filter

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,25 +2,32 @@ import React from "react"
 import ExpenseItem from "./ExpenseItem"
 import Card from "./Card"
 import "./Expenses.css"
+import ExpensesFilter from "./NewExpense/ExpensesFilter"
 const Expenses = (props) => {
+	const filterChangeHandler = (selectedYear) => {
+		console.log(selectedYear)
+	}
 	return (
-		<Card className='expenses'>
-			<ExpenseItem
-				title={props.items[0].title}
-				amount={props.items[0].amount}
-				date={props.items[0].date}
-			/>
-			<ExpenseItem
-				title={props.items[1].title}
-				amount={props.items[1].amount}
-				date={props.items[1].date}
-			/>
-			<ExpenseItem
-				title={props.items[2].title}
-				amount={props.items[2].amount}
-				date={props.items[2].date}
-			/>
-		</Card>
+		<div>
+			<ExpensesFilter onChangeFilter={filterChangeHandler} />
+			<Card className='expenses'>
+				<ExpenseItem
+					title={props.items[0].title}
+					amount={props.items[0].amount}
+					date={props.items[0].date}
+				/>
+				<ExpenseItem
+					title={props.items[1].title}
+					amount={props.items[1].amount}
+					date={props.items[1].date}
+				/>
+				<ExpenseItem
+					title={props.items[2].title}
+					amount={props.items[2].amount}
+					date={props.items[2].date}
+				/>
+			</Card>
+		</div>
 	)
 }
 
