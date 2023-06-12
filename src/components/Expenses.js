dbe8ce701e8b708c32f73a90a0e754cb0e04b5e9
irinamihai150 <irinamihai150@gmail.com>
2023-06-12 11:51:27 +0100

    render dinamic list

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,6 +3,7 @@ import ExpenseItem from "./ExpenseItem"
 import Card from "./Card"
 import "./Expenses.css"
 import ExpensesFilter from "./NewExpense/ExpensesFilter"
+
 const Expenses = (props) => {
 	const [filteredYear, setFilteredYear] = useState("2020")
 	const filterChangeHandler = (selectedYear) => {
@@ -10,26 +11,18 @@ const Expenses = (props) => {
 	}
 	return (
 		<div>
-			<ExpensesFilter
-				selected={filteredYear}
-				onChangeFilter={filterChangeHandler}
-			/>
 			<Card className='expenses'>
-				<ExpenseItem
-					title={props.items[0].title}
-					amount={props.items[0].amount}
-					date={props.items[0].date}
+				<ExpensesFilter
+					selected={filteredYear}
+					onChangeFilter={filterChangeHandler}
 				/>
-				<ExpenseItem
-					title={props.items[1].title}
-					amount={props.items[1].amount}
-					date={props.items[1].date}
-				/>
-				<ExpenseItem
-					title={props.items[2].title}
-					amount={props.items[2].amount}
-					date={props.items[2].date}
-				/>
+				{props.items.map((expense) => (
+					<ExpenseItem
+						title={expense.title}
+						amount={expense.amount}
+						date={expense.date}
+					/>
+				))}
 			</Card>
 		</div>
 	)
